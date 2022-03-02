1eeb77531841cebd137364da1071d3bf9f397091
ChrisZili <kiklop421@gmail.com>
2022-03-02 15:39:04 +0100

    getter and setter

--- src/main/java/auto/Auto.java
+++ src/main/java/auto/Auto.java
@@ -1,8 +1,8 @@
 package auto;
 
 public class Auto {
-    public double kapacitaNadrze;
-    public double stavNadrze;
+    private double kapacitaNadrze;
+    private double stavNadrze;
 
     public void jazdi(double vzdialenost){
         stavNadrze -= vzdialenost * 0.2;
@@ -11,6 +11,12 @@ public class Auto {
         }
     }
 
+    public double getStavNadrze(){
+        return stavNadrze;
+    }
 
+    public void setStavNadrze(double stavNadrze){
+        this.stavNadrze = stavNadrze;
+    }
 
 }
